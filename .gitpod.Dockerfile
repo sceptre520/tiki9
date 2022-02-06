d9ada000c10824bb345b5a6fed494d43df6c3bf0
sceptre520 <sceptre520@gmail.com>
2022-02-06 22:32:27 +0000

    []

---
+++ .gitpod.Dockerfile
@@ -0,0 +1,21 @@
+FROM gitpod/workspace-base:latest
+
+USER root
+
+RUN apt-get update && apt-get -y install apache2 sqlite rsync mysql-server tesseract-ocr php5.3 libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-intl php-bcmath php-zip php-bz2 php-imagick php-pdo php-sqlite3 php-dev php-pear
+
+RUN pecl install yamL
+RUN sh -c "echo 'extension=yaml.so' >> /etc/php/5.3/mods-available/yaml.ini"
+RUN phpenmod yaml
+
+RUN echo "include /workspace/lamp/apache/apache.conf" > /etc/apache2/apache2.conf
+RUN echo ". /workspace/lamp/apache/envvars" > /etc/apache2/envvars
+
+RUN echo "!include /workspace/lamp/mysql/mysql.cnf" > /etc/mysql/my.cnf
+
+RUN mkdir /var/run/mysqld
+RUN chown gitpod:gitpod /var/run/apache2 /var/lock/apache2 /var/run/mysqld
+
+RUN sudo sed -i 's/^bind-address.*/#&/' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+RUN addgroup gitpod www-data
---
+++ modules/mod-func-menupage.php
@@ -0,0 +1,47 @@
+<?php
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
+  header("location: index.php");
+  exit;
+}
+
+function module_menupage_info()
+{
+	return array(
+		'name' => tra('Menu Page'),
+		'description' => tra('Displays a Wiki page.'),
+		'prefs' => array('feature_wiki'),
+		'params' => array(
+			'pagemenu' => array(
+				'name' => tra('Page'),
+				'description' => tra('Page to display in the menu. Example value: HomePage.'),
+				'required' => true
+			)
+		)
+	);
+}
+
+function module_menupage($mod_reference, $module_params)
+{
+	global $smarty;
+	$pagemenu = $module_params['pagemenu'];
+	
+	if (!empty($pagemenu)) {
+		global $wikilib; include_once('lib/wiki/wikilib.php');
+		$perms = Perms::get(array('object' => $pagemenu, 'type' => 'wiki page'));
+
+		if ($perms->view) {
+			$content = $wikilib->get_parse($pagemenu, $dummy, true);
+		} else {
+			$content = '<label class="error">' . tra("You are not logged in") . '</label>';
+		}
+		$smarty->assign('tpl_module_title', $pagemenu);
+		$smarty->assign_by_ref('contentmenu', $content);
+		$smarty->assign('pagemenu', $pagemenu);
+	}
+}
